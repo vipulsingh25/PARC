25390e93509af58053fed7a82976a0e2ae204622
Vipul Singh <85404156+vipulsingh25@users.noreply.github.com>
2022-05-13 21:58:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize:{
+        '10xl': ['0.9375rem', { lineHeight: '1' }],
+        '11xl': ['2rem', {lineHeight: '1.2em'}],
+      },
+      letterSpacing:{
+        widewide : '0.125rem',
+      },
+      fontFamily:{
+        cero:'Cera PRO',
+      }
+    },
+  },
+  plugins: [],
+}
